5862db6c140b00927131005a1afb8a51478c19c8
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2021-06-03 09:41:33 -0300

    000.setup_projeto

---
+++ src/main/java/br/com/proposta/PropostaOrange/PropostaOrangeApplication.java
@@ -0,0 +1,13 @@
+package br.com.proposta.PropostaOrange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostaOrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaOrangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/proposta/PropostaOrange/PropostaOrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.proposta.PropostaOrange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaOrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
