3c954dd4ff7dc3a175dd11b1c3c602b5aac4fd08
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2021-06-09 15:24:11 -0300

    095.falhar_nas_chamadas

--- src/main/java/br/com/proposta/PropostaOrange/PropostaOrangeApplication.java
+++ src/main/java/br/com/proposta/PropostaOrange/PropostaOrangeApplication.java
@@ -1,9 +1,13 @@
 package br.com.proposta.PropostaOrange;
-
+import io.opentracing.Tracer;
+import io.opentracing.tag.Tags;
+import io.opentracing.util.GlobalTracer;
+import org.aspectj.weaver.tools.Trace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -13,4 +17,20 @@ public class PropostaOrangeApplication {
 		SpringApplication.run(PropostaOrangeApplication.class, args);
 	}
 
+	/*
+
+	@Bean
+	public io.opentracing.Tracer jaegerTracer() {
+		return new Configuration("propostas", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+				new Configuration.ReporterConfiguration())
+				.getTracer();
+	}
+
+	private Object getTracer(){
+		Tracer tracer = GlobalTracer.get();
+		Tracer.SpanBuilder spanBuilder = tracer.buildSpan("Custom")
+				.withTag(Tags.SPAN_KIND.getKey(),Tags.SPAN_KIND_SERVER);
+	}
+
+	 */
 }
